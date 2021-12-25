1f2b95daee66b216258b6da1363d16b5ed5dedbe
Sahil Shende <ssahilshende@gmail.com>
2021-12-25 13:48:09 +0530

    create textbox to add new note

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,11 +3,13 @@ import Header from "./Header";
 import Note from "./Note";
 import Footer from "./Footer";
 import notes from "../notes";
+import CreateArea from "./CreateArea";
 
 function App() {
   return (
     <div>
       <Header />
+      <CreateArea />
       {notes.map((note) => (
         <Note key={note.id} title={note.title} content={note.content} />
       ))}
